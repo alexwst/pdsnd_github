b8302e31b294fa8785d3a84c7740c1a067b1fa79
Alex Steil <awesleysteil@gmail.com>
2020-08-23 15:00:15 -0600

    Added comment on city_data

--- bikeshare_rev.py
+++ bikeshare_rev.py
@@ -3,6 +3,7 @@ import time
 import pandas as pd
 import numpy as np
 
+# linking files to city names
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
